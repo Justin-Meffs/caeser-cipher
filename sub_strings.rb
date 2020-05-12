e171ee87551ae667ef1c1603e8a759e9d210be9f
Justin Mepham <justin0mepham@gmail.com>
2020-05-12 16:40:01 -0600

    Complete

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,14 +1,17 @@
 def substrings()
-    user_word = 'below'
+    text = "Howdy partner, sit down! How's it going?"
     dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
     hash = Hash.new(0)
-    
+    text = String(text).downcase
+    user_string_array = text.split(' ')
+
     for word in dictionary
-        if user_word.include?(word)
-            hash[word] += 1
+        for s_word in user_string_array
+            if s_word.include?(word)
+                hash[word] += 1
+            end
         end
     end
     puts hash
-
 end
 substrings()
