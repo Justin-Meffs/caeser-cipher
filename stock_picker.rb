e70435285eda3207ab4504ad9a9960ebd6016a81
Justin Mepham <justin0mepham@gmail.com>
2020-05-23 19:19:23 -0600

    stuck in infinite loop

---
+++ stock_picker.rb
@@ -0,0 +1,36 @@
+def stock_picker(array)
+	profit = 0
+	buy_index = 0
+	sell_index = 1
+	best_sell_day = 0
+	best_buy_day = 0
+	
+	while buy_index != array.length - 1 
+		if array[sell_index] - array[buy_index] > profit
+			profit = array[sell_index] - array[buy_index]
+			best_buy_day = buy_index
+			best_sell_day = sell_index
+			sell_index += 1
+		
+			                 
+			if sell_index == array.length 
+				buy_index += 1
+				sell_index = buy_index + 1
+			end
+		end
+	end
+	
+	if profit <= 0
+		puts "Every combination is a loss."
+	else
+		best_days = []
+		best_days << best_buy_day
+		best_days << best_sell_day
+		print best_days
+	end
+end
+        
+
+stock_picker([10,11,12,13,14,15])
+
+
