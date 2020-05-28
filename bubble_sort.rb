d90db4643f2fc60883a7894138521825307f9377
Justin Mepham <justin0mepham@gmail.com>
2020-05-28 06:57:45 -0600

    finished

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,34 +1,25 @@
 def bubble_sort(array)
-	first_num = 0
-	second_num = 1
-	temp_var = 0
-	counter = 0
-	#number of iterations needed to sort the entire array
-	while counter !=  array.length - 1
-		#if you are at the end of the list then start over
-		if first_num == array.length - 1
-			first_num = 0
-			second_num = 1
-		end
-
-		for num in array
-			#swap elements if left is bigger than right then move to next
-			#elements
-			if array[first_num] > array[second_num]
-				temp_var = array[first_num]
-				array[first_num] = array[second_num]
-				array[second_num] = temp_var
-				first_num += 1
-				second_num += 1
-			#if left isn't bigger than right: move onto next elements
-			else
-				first_num += 1
-				second_num += 1
-			end
-			counter += 1
-			print array 
-			print "\n"
-		end
+	left = 0
+	right = 1
+	count = 0
+  
+	while count < array.length - 1
+	  if array[left] > array[right]
+		temp = array[left]
+		array[left] = array[right]
+		array[right] = temp
+	  end
+	  left += 1
+	  right += 1
+	  #once you get to the end of the array start a new iteration
+	  if right == array.length
+		left = 0
+		right = 1
+		count += 1
+	  end
 	end
+	print array
+	puts " "
 end
+
 bubble_sort([1,5,2,7,6,3,8,5])
