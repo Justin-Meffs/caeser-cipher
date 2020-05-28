ef16766fc565c3dcd9574922a19b53a93a79576f
Justin Mepham <justin0mepham@gmail.com>
2020-05-28 02:29:40 -0600

    have a bug, going to completely restart on second attempt

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,19 +2,33 @@ def bubble_sort(array)
 	first_num = 0
 	second_num = 1
 	temp_var = 0
+	counter = 0
+	#number of iterations needed to sort the entire array
+	while counter !=  array.length - 1
+		#if you are at the end of the list then start over
+		if first_num == array.length - 1
+			first_num = 0
+			second_num = 1
+		end
 
-	for num in array
-		/*swap elements if left is bigger than right then move to next
-		elements*/
-		if array[first_num] > array[second_num]
-			temp_var = array[first_num]
-			array[frist_num] = array[second_num]
-			array[second_num] = temp_var
-			first_num += 1
-			second_num += 1
-		/*if left isn't bigger than right: move onto next elements*/
-		else
-			first_num += 1
-			second_num += 1
-		
-
+		for num in array
+			#swap elements if left is bigger than right then move to next
+			#elements
+			if array[first_num] > array[second_num]
+				temp_var = array[first_num]
+				array[first_num] = array[second_num]
+				array[second_num] = temp_var
+				first_num += 1
+				second_num += 1
+			#if left isn't bigger than right: move onto next elements
+			else
+				first_num += 1
+				second_num += 1
+			end
+			counter += 1
+			print array 
+			print "\n"
+		end
+	end
+end
+bubble_sort([1,5,2,7,6,3,8,5])
