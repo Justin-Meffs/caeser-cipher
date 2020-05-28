bd99c574db5ae35aff86b4a9e1b925b3ff5d2c2e
Justin Mepham <justin0mepham@gmail.com>
2020-05-28 06:07:49 -0600

    cleaned up code, everything is working

--- stock_picker3.rb
+++ stock_picker3.rb
@@ -3,23 +3,18 @@ def stock_picker(array)
     sell_day = 1
     profit = 0
 
-    while buy_day != array.length - 1
-        print array[buy_day]
-        puts array[sell_day] 
+    while buy_day != array.length - 1 
         if array[sell_day] - array[buy_day] > profit
             profit = array[sell_day] - array[buy_day]
             best_buy_day = buy_day
             best_sell_day = sell_day
         end
-        puts profit
         sell_day += 1
         if sell_day == array.length
             buy_day += 1
             sell_day = buy_day + 1
         end
     end
-    puts best_buy_day
-    puts best_sell_day
     if profit == 0
         puts "There is no good day to buy"
     else
@@ -27,6 +22,7 @@ def stock_picker(array)
         best_days << best_buy_day 
         best_days << best_sell_day
         print best_days
+        puts " "
     end
 end
-stock_picker([1,2,3,4,5])
+stock_picker([7,13,3,17,19,347,5,43,20,0,500,1000])
