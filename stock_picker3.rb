831590e0690131f4366ee718fc8ac8e5b0675f48
Justin Mepham <justin0mepham@gmail.com>
2020-05-28 05:58:15 -0600

    correctly iterating, correct profit, correct best days to buy and sell

--- stock_picker3.rb
+++ stock_picker3.rb
@@ -1,14 +1,24 @@
 def stock_picker(array)
     buy_day = 0
     sell_day = 1
+    profit = 0
+
     while buy_day != array.length - 1
         print array[buy_day]
-        print array[sell_day]
+        puts array[sell_day] 
+        if array[sell_day] - array[buy_day] > profit
+            profit = array[sell_day] - array[buy_day]
+            best_buy_day = buy_day
+            best_sell_day = sell_day
+        end
+        puts profit
         sell_day += 1
         if sell_day == array.length
             buy_day += 1
             sell_day = buy_day + 1
         end
     end
+    puts best_buy_day
+    puts best_sell_day
 end
 stock_picker([1,2,3,4,5])
